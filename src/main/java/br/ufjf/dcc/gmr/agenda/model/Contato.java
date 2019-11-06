834fc55b73b472586522ba28a9f02364b83572db
Gleiph <gleiphgh@gmail.com>
2019-11-06 16:46:34 -0200

    Adicionando classe de model

---
+++ src/main/java/br/ufjf/dcc/gmr/agenda/model/Contato.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.agenda.model;
+
+/**
+ *
+ * @author gleiph
+ */
+public class Contato {
+    
+    private String nome;
+    private String telefone;
+    private String detalhe;
+
+    public Contato(String nome, String telefone, String detalhe) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.detalhe = detalhe;
+    }
+
+    public Contato() {
+        this.nome = "";
+        this.telefone = "";
+        this.detalhe = "";
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDetalhe() {
+        return detalhe;
+    }
+
+    public void setDetalhe(String detalhe) {
+        this.detalhe = detalhe;
+    }
+    
+}
